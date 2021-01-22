892b53ace4d41936028094de6c7a5590a291eaa4
Achew <artem.kovzik@yandex.ru>
2021-01-22 21:38:24 +0300

    Выполнил нормально 1 сложное задание

--- Practice_3.2_hard.py
+++ Practice_3.2_hard.py
@@ -6,23 +6,38 @@
 # они получают удвоенную ЗП, пропорциональную норме.
 # Кол-во часов, которые были отработаны, указаны в файле "data/hours_of"
 import re
-rate = 200
-normative = 40
-regex = r'(?P<first_name>\S+) +(?P<last_name>\S+) +(?P<hours>\d+)'
-with open('hours_of', encoding='utf-8') as r:
-    next(r)
-    for read in r:
-        match = re.search(regex, read)
-        print(match.groups())
-        """with open('workers', 'w', encoding='utf-8') as w:
-            for employee, hours in reader.items():
-                hours = int(hours)
-                if 0 < hours <= normative:
-                    w.write(f'{employee} - {hours * rate} евро\n')
-                elif normative < hours < 252:
-                    w.write(f'{employee} - {normative * rate + (hours - normative) * rate * 2} евро\n')
-                elif hours >= 252:
-                    w.write(f'{employee} скорее всего мертв, так как нельзя отработать больше {hours} часов в неделю\n')
-                elif hours <= 0:
-                    w.write(f'{employee} не работал вовсе или смог задолжать компании каким-то образом\n')
-"""
+
+
+def hours_worked(file):
+    regex_h = r'(?P<Имя>\S+) +(?P<Фамилия>\S+) +(?P<Отработано_часов>\d+)'
+    h_w = []
+    with open(file, encoding='utf-8') as hr:
+        next(hr)
+        for hour_read in hr:
+            match_h = re.search(regex_h, hour_read)
+            h_w.append(match_h.groups())
+    return h_w
+
+
+def normative_hours(file):
+    regex_w = r'(?P<Имя>\S+) +(?P<Фамилия>\S+) +(?P<Зарплата>\d+) +(?P<Должность>\S+) +(?P<Норма_часов>\d+)'
+    workers = []
+    with open(file, encoding='utf-8') as wr:
+        next(wr)
+        for worker_read in wr:
+            match_w = re.search(regex_w, worker_read)
+            workers.append(match_w.groups())
+    return workers
+
+print(f'{"Имя":<10} {"Фамилия":<10} {"Отработано_часов":<5} {"Норматив":<5} {"Зарплата":<5}\n')
+for h_workers in hours_worked('hours_of'):
+    rate = int(h_workers[2])
+    for compare in normative_hours('workers'):
+        if h_workers[0] and h_workers[1] in compare:
+            money = int(compare[2])
+            norm = int(compare[4])
+            if rate > norm:
+                pay = int((2 * money / norm) * (rate - norm) + money)
+            else:
+                pay = int(money / norm * rate)
+            print(f'{h_workers[0]:<10} {h_workers[1]:<10} {rate:<16} {norm:<8} {pay:<5}')
